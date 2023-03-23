db76887298398c2f79ee0a0f91e1fd25d7f95805
finja.stiepermann <finja.stiepermann@eckcellent-it.de>
2023-03-23 17:36:59 +0100

    Comment

--- src/main/kotlin/sudoku_transform_solver/model/transformation/Transformation.kt
+++ src/main/kotlin/sudoku_transform_solver/model/transformation/Transformation.kt
@@ -1,5 +1,8 @@
 package sudoku_transform_solver.model.transformation
 
+/**
+ * Wraps sudoku transformation into objects to organize transformation sequenzes into data structures
+ */
 interface Transformation {
     fun apply();
     override fun toString(): String;
