f45e902e9e60747867391b21bb0e871a67ce8324
finja.stiepermann <finja.stiepermann@eckcellent-it.de>
2023-03-23 17:29:37 +0100

    Setup Kotlin spring Applikation

---
+++ src/main/kotlin/sudoku_transform_api/STApiRestSpringServer.kt
@@ -0,0 +1,12 @@
+package sudoku_transform_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class STApiRestSpringServer
+
+fun main(args: Array<String>) {
+    runApplication<STApiRestSpringServer>(*args)
+}
---
+++ src/main/kotlin/sudoku_transform_solver/ConsoleApplikation.kt
@@ -0,0 +1,12 @@
+package sudoku_transform_solver
+
+fun main(args: Array<String>) {
+
+}
+
+fun parseInput(input: String) {
+    if(input == "turn l") { return; }
+    if(input == "turn r") { return; }
+
+
+}
