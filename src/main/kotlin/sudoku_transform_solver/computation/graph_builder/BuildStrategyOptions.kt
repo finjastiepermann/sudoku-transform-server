f2f1abf1e78c83558a3e0a63a3ccaed816c0857c
finja.stiepermann <finja.stiepermann@eckcellent-it.de>
2023-05-11 14:00:22 +0200

    Implemented Graph Builder

--- src/main/kotlin/sudoku_transform_solver/computation/graph_builder/BuildStrategyOptions.kt
+++ src/main/kotlin/sudoku_transform_solver/computation/graph_builder/BuildStrategyOptions.kt
@@ -1,9 +1,10 @@
 package sudoku_transform_solver.computation.graph_builder
 
+const val DEFAULT_MAX_DEPTH : Int = 20;
 class BuildStrategyOptions {
-    var maxDepth : Int? = null;
+    var maxDepth : Int = DEFAULT_MAX_DEPTH;
 
-    fun maxDepth(n : Int?) : BuildStrategyOptions {
+    fun maxDepth(n : Int) : BuildStrategyOptions {
         this.maxDepth = n;
         return this;
     }
--- src/main/kotlin/sudoku_transform_solver/computation/graph_builder/GraphBuilder.kt
+++ src/main/kotlin/sudoku_transform_solver/computation/graph_builder/GraphBuilder.kt
@@ -5,18 +5,31 @@ import sudoku_transform_solver.model.graph.Graph
 class GraphBuilder<T, U>(
     private val strategy : BuildStrategy<T, U>,
     private val hashFunction: (c : T) -> Int,
-    private val options : BuildStrategyOptions
+    private val options : BuildStrategyOptions = BuildStrategyOptions()
     ) {
 
-    fun build(start : T, target : T) : Graph<T, U> {
+    fun build(start : T, target : T) : Graph<T, U>? {
+        if  (start == null || target == null) return null;
         val graph = Graph<T, U>(this.hashFunction);
 
         graph.add(start);
-        return buildInternal(start, target, graph, 20, null);
+        buildInternal(start, target, graph, options.maxDepth, null);
+        return graph;
     }
 
-    fun buildInternal(current: T, target: T, graph: Graph<T, U>, depth: Int, previous : T?) : Graph<T, U> {
-        //TODO
-        return graph;
+    private fun buildInternal(current: T, target: T, graph: Graph<T, U>, depth: Int, previous : T?) {
+        if (current == null || target == null) return;
+        if (depth <= 0) return;
+        if (graph.getAdj(current)!!.size > 0) return;
+        if (current.equals(target))  return;
+
+        val adj = this.strategy.next(current, target, graph, previous, depth);
+
+        adj.forEach { e ->
+            graph.add(e.first);
+            graph.connect(current, e.first, e.second, false);
+
+            this.buildInternal(e.first, target, graph, depth - 1, current);
+        }
     }
 }
